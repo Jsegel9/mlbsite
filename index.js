e4d97e9438562f973c51024c99bb23207d71fe17
Jsegel <segeljosh@gmail.com>
2020-02-10 14:39:16 -0600

    server running

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+var path = require("path")
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+var routes = require("./mlbcontroller"); 
+app.use(routes);
+app.listen(PORT, function(){
+    console.log("Server listening on http://localhost:" + PORT);
+});
---
+++ mlbcontroller.js
@@ -0,0 +1,9 @@
+const express = require("express");
+var router = express.Router();
+var path = require("path")
+
+router.get("/", function(req,res){
+    res.sendfile(path.join(__dirname + "/views/index.html"));
+})
+
+module.exports = router;
